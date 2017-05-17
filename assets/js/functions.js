4cc8d4a7d521a6400deba090da49599b6c16017f
Luis Benitez <luisbttf@gmail.com>
2017-05-17 14:53:51 -0500

    blog

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,4 +3,33 @@ $(document).ready(function () {
     $(".full").toggleClass( "none" );
     $("section.menu").toggleClass( "solid" );
   });
+ //////////////////////////////////      1     /////////////////////////////////////
+  //Blog leer mas
+  $("article:nth-child(1) .leermas").on("click",function(){
+    $("article:nth-child(1) .contentmas").slideDown(1000)
+    $("article:nth-child(1) .leermas").css("display","none");
+    $("article:nth-child(1) .leermenos").css("display","block");
+  });
+  //Blog leer menos
+  $("article:nth-child(1) .leermenos").on("click",function(){
+    $("article:nth-child(1) .contentmas").slideUp(1000)
+    $("article:nth-child(1) .leermas").css("display","block");
+    $("article:nth-child(1) .leermenos").css("display","none");
+  });
+
+   //////////////////////////////////      2     /////////////////////////////////////
+
+  //Blog leer mas
+  $("article:nth-child(2) .leermas").on("click",function(){
+    $("article:nth-child(2) .contentmas").slideDown(1000)
+    $("article:nth-child(2) .leermas").css("display","none");
+    $("article:nth-child(2) .leermenos").css("display","block");
+  });
+  //Blog leer menos
+  $("article:nth-child(2) .leermenos").on("click",function(){
+    $("article:nth-child(2) .contentmas").slideUp(1000)
+    $("article:nth-child(2) .leermas").css("display","block");
+    $("article:nth-child(2) .leermenos").css("display","none");
+  });
+
 });
